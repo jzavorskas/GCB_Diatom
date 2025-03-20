ca9bf278054bd43d7fcdc7f3db4a381b3ad06f9d
Joseph Zavorskas <92336939+jzavorskas@users.noreply.github.com>
2025-03-20 08:23:18 -0400

    Fixed sensitivity analysis code

--- SensitivityAnalysis.py
+++ SensitivityAnalysis.py
@@ -36,7 +36,7 @@ from time import time
 sys.path.append("C:\\Users\\josep\\OneDrive - University of Connecticut\\GRADUATE SCHOOL\\Srivastava Group\\~Organized Grad School\\Ch.6 dFBA Model\\VSCode Port\\Code_NoDepth")
 sys.path.append("C:\\Users\\josep\\OneDrive - University of Connecticut\\GRADUATE SCHOOL\\Srivastava Group\\~Organized Grad School\\Ch.6 dFBA Model\\VSCode Port\\Code_NoDepth\\Auxiliary_Functions")
 
-from Code_NoDepth.Auxiliary_Functions import Light, Extras, Input, LoadModel, MergeLoadModel
+from Auxiliary_Functions import Light, Extras, Input, LoadModel, MergeLoadModel
 
 # Initialize the inputs dictionary which will be used through dFBA simulation.
 [Inputs, start_date] = Input.Input()
@@ -47,8 +47,8 @@ Scenario = ["RuBisCo","Nitrogen","Silicon","Iron"]
 LightType = ["Day","Night"]
 
 NitrogenDay = np.array([[9,0.25,9,9],[9,0.25,9,9]])
-SiliconDay = np.array([[4,0.25,4,4],[4,0.25,4,4]])
-IronDay = np.array([[0.004,0.0001,0.004,0.004],[0.004,0.0001,0.004,0.004]])
+SiliconDay = np.array([[4,4,0.25,4],[4,4,0.25,4]])
+IronDay = np.array([[0.004,0.004,0.004,0.0001],[0.004,0.004,0.004,0.0001]])
 T = 5 # degrees Celsius
 
 # Calculate the saturation concentration of carbon dioxide (in mM).
@@ -58,6 +58,9 @@ O2sat = Henry[1]*(.2105) # convert to uM
 N2sat = Henry[2]*(.781)
 n = 1.9
 
+starttime = time()
+Iphoto = Inputs['Iphoto']
+
 for idy, Nutrient in enumerate(Scenario):
 
     for idx, Type in enumerate(LightType):
@@ -65,10 +68,38 @@ for idy, Nutrient in enumerate(Scenario):
         print("Nutrient:", Nutrient)
         print("Type:", Type)
 
-        ML.reactions.EX_co2_e.upper_bound = 1000
-        ML.reactions.EX_co2_e.lower_bound = -Extras.RLPD(CO2sat*4,O2sat,T)
-        DML.reactions.EX_co2_e.upper_bound = 1000
-        DML.reactions.EX_co2_e.lower_bound = -Extras.RLPD(CO2sat*4,O2sat,T)
+        if idx == 0:
+
+            for k in Iphoto.keys():
+                DML.reactions.get_by_id(k).upper_bound = 0
+                DML.reactions.get_by_id(k).lower_bound = -1
+                DML.reactions.get_by_id(k).upper_bound = -0.9999
+                ML.reactions.get_by_id(k).upper_bound = 0
+                ML.reactions.get_by_id(k).lower_bound = -1
+                ML.reactions.get_by_id(k).upper_bound = -0.9999
+
+            ML.reactions.EX_co2_e.upper_bound = 0
+            ML.reactions.EX_co2_e.lower_bound = -Extras.RLPD(CO2sat*4,O2sat,T)
+            ML.reactions.EX_chryso_e.lower_bound = 0
+            DML.reactions.EX_chryso_e.lower_bound = 0
+            DML.reactions.EX_co2_e.upper_bound = 0
+            DML.reactions.EX_co2_e.lower_bound = -Extras.RLPD(CO2sat*4,O2sat,T)
+        
+        else:
+
+            for k in Iphoto.keys():
+                DML.reactions.get_by_id(k).upper_bound = 0
+                DML.reactions.get_by_id(k).lower_bound = Iphoto[k]*-1.
+                DML.reactions.get_by_id(k).upper_bound = Iphoto[k]*-0.9999
+                ML.reactions.get_by_id(k).upper_bound = 0
+                ML.reactions.get_by_id(k).lower_bound = Iphoto[k]*-1.
+                ML.reactions.get_by_id(k).upper_bound = Iphoto[k]*-0.9999
+
+            DML.reactions.EX_chryso_e.lower_bound = -0.5
+            ML.reactions.EX_chryso_e.lower_bound = -0.5
+            DML.reactions.EX_co2_e.lower_bound = 0
+            ML.reactions.EX_co2_e.lower_bound = 0
+            ML.reactions.get_by_id("EX_cpd00011[e]").lower_bound = 0
 
         ML.reactions.EX_no3_e.lower_bound = -((1.474*NitrogenDay[idx,idy])/(6.14+NitrogenDay[idx,idy]))
         DML.reactions.EX_no3_e.lower_bound = -((1.474*NitrogenDay[idx,idy])/(6.14+NitrogenDay[idx,idy]))
@@ -78,13 +109,21 @@ for idy, Nutrient in enumerate(Scenario):
 
         ML.reactions.get_by_id("EX_cpd10515[e]").lower_bound = -((0.0024*IronDay[idx,idy])/(0.0023+IronDay[idx,idy]))
 
+        DML.optimize()
+        ML.optimize()
+
+        print("Diatom-Only")
         FVADML = cobra.flux_analysis.variability.flux_variability_analysis(DML)
+        print("Symbiotic")
         FVAML = cobra.flux_analysis.variability.flux_variability_analysis(ML)
 
-        DMLFileName = ["DiatomOnly_FVA_"+Nutrient+"Limit_"+Type+".csv"]
-        MLFileName = ["Symbiotic_FVA_"+Nutrient+"Limit_"+Type+".csv"]
+        DMLFileName = "./SensitivityOutputs/DiatomOnly_FVA_"+Nutrient+"Limit_"+Type+".csv"
+        MLFileName = "./SensitivityOutputs/Symbiotic_FVA_"+Nutrient+"Limit_"+Type+".csv"
 
         FVADML.to_csv(DMLFileName, index=True) 
-        FVAML.to_csv(DMLFileName, index=True) 
+        FVAML.to_csv(MLFileName, index=True) 
 
-        print("Completion:", (((idy*4+idx)+1)/8))
+        print("Completion:", (((idy*4+idx)+1)/8)*100,"%")
+        print("Elapsed Time:", (time() - starttime))
+
+        
